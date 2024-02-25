ffecb56e903fe0e40d40d4b1f94ac200b7b377b2
Alyssia <115212411+alyssialopr@users.noreply.github.com>
2024-02-25 19:36:22 +0100

    changement !

--- back/router.js
+++ back/router.js
@@ -1,17 +1,7 @@
-// const express = require('express');
-// const champions = require('./routes/champions');
-
-// const router = express.Router();
-
-// router.use('/champions', champions);
-
-// export default router;
-
 import express from 'express';
-import Champion from './routes/champions';
+import champions from './routes/ChampionsRoutes.js';
+const router = express.Router();
+ 
+router.use('/champions', champions);
 
-const championsRouter = express.Router();
-
-// Définissez vos routes pour les champions ici
-
-export default championsRouter;
+export default router;
